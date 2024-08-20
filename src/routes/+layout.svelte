47a65f27e9a2d75cd47c4a9b7f4a1c69f333f79d
Jonas Claes <jonas@jonasclaes.be>
2024-08-20 11:56:12 +0000

    feat: removed demo stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,43 +11,6 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>A project by <a href="https://jonasclaes.be" target="_blank">Jonas Claes</a>.</p>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
