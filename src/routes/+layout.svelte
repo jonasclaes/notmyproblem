6859dcc5988669e022001604c44697eaf9d56d3e
Jonas Claes <jonas@jonasclaes.be>
2024-08-21 07:32:59 +0000

    feat: add link to GitHub repository

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,18 @@
 
 	<footer class="footer footer-center bg-neutral text-neutral-content p-4">
 		<aside>
-			<p>A project by <a href="https://jonasclaes.be" target="_blank" class="text-blue-400">Jonas Claes</a>.</p>
+			<p>
+				A project by <a href="https://jonasclaes.be" target="_blank" class="text-blue-400"
+					>Jonas Claes</a
+				>.
+			</p>
+			<p>
+				Check out the source code on GitHub! <a
+					href="https://github.com/jonasclaes/notmyproblem"
+					target="_blank"
+					class="text-blue-400">jonasclaes/notmyproblem</a
+				>.
+			</p>
 			<p>Copyright © {new Date().getFullYear()}</p>
 		</aside>
 	</footer>
