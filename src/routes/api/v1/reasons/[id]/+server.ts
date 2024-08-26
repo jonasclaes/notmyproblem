19e445af58e84e50b4036442fa2c8b3c46de2fc1
Jonas Claes <jonas@jonasclaes.be>
2024-08-26 20:59:33 +0000

    feat: add endpoint to lookup unique reason

---
+++ src/routes/api/v1/reasons/[id]/+server.ts
@@ -0,0 +1,22 @@
+import { eq } from 'drizzle-orm';
+import type { RequestHandler } from './$types';
+import { notMyProblemsTable } from '$lib/server/db/schema';
+
+export const GET: RequestHandler = async ({ url, locals: { db }, params }) => {
+	const id = parseInt(params.id);
+
+	const results = await db.query.notMyProblemsTable.findMany({
+		where: eq(notMyProblemsTable.id, id)
+	});
+
+	return Response.json(
+		{
+			data: results,
+			_links: {
+				base: url.origin,
+				self: url.href
+			}
+		},
+		{ status: 200 }
+	);
+};
