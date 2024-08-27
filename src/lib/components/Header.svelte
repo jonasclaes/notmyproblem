f28509e688cc11a357791e046df8bf3881a0486c
Jonas Claes <jonas@jonasclaes.be>
2024-08-27 11:55:01 +0000

    fix: hide get started button in navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -40,8 +40,8 @@
 				{/each}
 			</ul>
 		</div>
-		<div class="navbar-end">
+		<!-- <div class="navbar-end">
 			<a href="/get-started" class="btn">Get started</a>
-		</div>
+		</div> -->
 	</div>
 </header>
