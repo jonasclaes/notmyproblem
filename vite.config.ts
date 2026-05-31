42fa32e6ee2d2d6931e158dde23d96a7fa0b0cf4
Jonas Claes <jonas@jonasclaes.be>
2026-05-31 16:21:13 +0200

    chore: clean up vite.config.ts by removing commented code

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [
-		// @ts-expect-error - No types available for this plugin
-		tailwindcss(),
-		sveltekit()
-	]
+	plugins: [tailwindcss(), sveltekit()]
 });
