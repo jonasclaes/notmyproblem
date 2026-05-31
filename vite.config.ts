7042141e0bc4d799f5472b157f2046b8cd0b5fad
Jonas Claes <jonas@jonasclaes.be>
2026-05-31 16:05:42 +0200

    chore: upgrade tailwind to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		// @ts-expect-error - No types available for this plugin
+		tailwindcss(),
+		sveltekit()
+	]
 });
